00b428545ba793c0ae0c78cd84256933816a8da7
Mohamedswahed <mohamedswahed@gmail.com>
2023-10-24 10:59:34 -0700

    square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" class Square"""
+
+
+class Square:
+    """defines square class"""
+    def __init__(self, size=0):
+        """ init square
+
+        Args:
+            value: size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """int: private size.
+
+        Returns:
+            Private size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value: size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+    def area(self):
+        """returns the area
+
+        Returns:
+            the area.
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """prints square with the character #"""
+
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
+        else:
+            print()
