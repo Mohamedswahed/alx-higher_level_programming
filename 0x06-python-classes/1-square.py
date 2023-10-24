3e5a46846bfa7022f68aecfaff39565f882102b6
Mohamedswahed <mohamedswahed@gmail.com>
2023-10-24 10:36:22 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square():
+    """Implementing square class"""
+    def __init__(self, size):
+        """ initialize class
+        Args:
+           size: size of the square
+        """
+        self.__size = size
