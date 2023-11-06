c4caf51ee3b2c37f5195e22c2271c467dcd4ca3e
Mohamedswahed <mohamedswahed@gmail.com>
2023-11-06 05:52:47 -0800

    square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,43 @@
 #!/usr/bin/python3
 """
-Module for BaseGeometry
+Contains the class BaseGeometry and subclass Rectangle
 """
 
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A representation of a rectangle"""
+    def __init__(self, width, height):
+        """instantiation of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
 class Square(Rectangle):
-    """A subclass representing rectangular"""
+    """A subclass of a square"""
     def __init__(self, size):
         """constructor of the square"""
         self.integer_validator("size", size)
@@ -12,9 +45,9 @@ class Square(Rectangle):
         super().__init__(size, size)
 
     def area(self):
-        """"method of  area of the square"""
+        """method area of the square"""
         return self.__size ** 2
 
     def __str__(self):
-        """string reepresenting of the square"""
+        """string reepresentation of the square"""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
