a12cdfc09c2a44ff36ca18f67bb3fed21458a277
Mohamedswahed <mohamedswahed@gmail.com>
2024-01-08 10:41:51 -0800

    Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-'use strict';
-let x = process.argv[2];
-function factorial (x) {
-  if (isNaN(x) || x === 1) {
-    return (1);
-  } else {
-    return (x * factorial(x - 1));
-  }
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
-console.log(factorial(parseInt(x)));
