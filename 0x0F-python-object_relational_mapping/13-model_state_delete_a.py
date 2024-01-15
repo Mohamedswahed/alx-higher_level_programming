78e7053529488c92e0b405b32c61b23286ae34e5
Mohamedswahed <mohamedswahed@gmail.com>
2024-01-14 17:12:18 -0800

    model_state_delete_a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-
 """
-Module to perfom simple queries on the model_state model
-using and ORM - SQLAlchemy
+Contains the class definition of a City
 """
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-def connect_and_query(user: str, passwd: str, dbase: str) -> None:
-
+class City(Base):
     """
-    Connect to the database and make queries using ORM
+    Class that defines each city
     """
-    try:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(user, passwd, dbase))
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        states = session.query(State).filter(State.name.like('%a%')).all()
-        [session.delete(state) for state in states]
-        session.commit()
-
-    except Exception as e:
-        return e
-
-
-if __name__ == "__main__":
-    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
